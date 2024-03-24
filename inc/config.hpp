ac6af787a77b62b0465b8537e55d620f492f2ef4
kiddersmxj <maxkiddmay@gmail.com>
2024-03-24 15:58:22 +0000

    intial commit

---
+++ inc/config.hpp
@@ -0,0 +1,21 @@
+#ifndef K_CONFIG_ASSIMILATE
+#define K_CONFIG_ASSIMILATE
+
+#include <iostream>
+#include <std-k.hpp>
+
+const std::string ProgramName = "assimilate";
+const std::string Version = "0.0.0";
+const std::string UsageNotes = R"(usage: assimilate [ -h/-v ]
+options:
+    -h / --help         show help and usage notes
+    -v / --version      print version and exit)";
+
+#endif
+
+// Copyright (c) 2023, Maxamilian Kidd-May
+// All rights reserved.
+
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree. 
+
